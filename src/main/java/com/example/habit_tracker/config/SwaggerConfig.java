e829496230061ca14c12d9fea1b4035cf4418e2e
saurabk0002 <samk799229@gmail.com>
2025-09-28 16:58:15 +0530

    Add SwaggerConfig with Render base URL

--- src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
+++ src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
@@ -1,31 +1,22 @@
 package com.example.habit_tracker.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Habit Tracker API")
-                        .version("1.0")
-                        .description("Swagger UI with JWT Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT") // 🔑 yahi JWT authorize button ko enable karega
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-    }
+        // Apna Render URL yaha daal
+        Server renderServer = new Server();
+        renderServer.setUrl("https://habit-tracker-service-c1qp.onrender.com");
+        renderServer.setDescription("Render Deployment Server");
+
+        return new OpenAPI().servers(List.of(renderServer));
+}
 }
