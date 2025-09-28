c0dfee0b0d223f3a809bbd7667ab866dd99f8c32
saurabk0002 <samk799229@gmail.com>
2025-09-28 18:33:04 +0530

    Fix Swagger + application.properties for Render

--- src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
+++ src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
@@ -1,22 +1,19 @@
 package com.example.habit_tracker.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        // Apna Render URL yaha daal
-        Server renderServer = new Server();
-        renderServer.setUrl("https://habit-tracker-service-c1qp.onrender.com");
-        renderServer.setDescription("Render Deployment Server");
-
-        return new OpenAPI().servers(List.of(renderServer));
-}
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Habit Tracker API")
+                        .version("1.0")
+                        .description("Swagger UI with JWT Authentication"));
+    }
 }
