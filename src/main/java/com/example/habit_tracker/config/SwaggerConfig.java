a72c4ae831f7b038b2b320b74ce4c12c1d307020
saurabk0002 <samk799229@gmail.com>
2025-12-10 21:52:45 +0530

    Prepare for Render deployment

--- src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
+++ src/main/java/com/example/habit_tracker/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.example.habit_tracker.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,17 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Habit Tracker API")
-                        .version("1.0")
-                        .description("Swagger UI with JWT Authentication"));
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
     }
 }
